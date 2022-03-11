9ed1cbfa9f9bce1af82c80e5fe4b3a23c581778d
Zachary Sirna <zsirna@yahoo.com>
2022-03-11 13:29:51 -0500

    commit tree 2065590c5671f00f44a8e8ee7bf957f04248521f

--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  getToys()
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
+   //add
+    })
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
     } else {
       toyFormContainer.style.display = "none";
     }
-  });
+    toyFormContainer.addEventListener('submit', (e) => {
+      e.preventDefault()
+      console.log(e.target.name.value, e.target.image.value)
+    })
 });
+
+
+function getToys(){
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(data => {
+    data.forEach(toy => renderToy(toy))
+  })
+}
+
+const toyBox = document.getElementById('toy-collection')
+
+function renderToy(toy){
+    const card = document.createElement("div")
+    const head = document.createElement("h2")
+    const image = document.createElement("img")
+    const pTag = document.createElement("p")
+    const likeButton = document.createElement("button")
+    head.innerText = toy.name
+    image.className = ("toy-avatar")
+    image.src = toy.image
+    pTag.innerText = toy.likes
+    card.className = "card"
+    likeButton.className = ("like-btn")
+    likeButton.innerText = "Likes"
+    
+    card.append(head, image, pTag, likeButton)
+    toyBox.appendChild(card)
+}
+
+function newToy(){
+
+}
